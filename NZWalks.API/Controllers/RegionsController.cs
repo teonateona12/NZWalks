e415143eaeb9b4bd7a2549430f206b1f54debd29
teonateona12 <teona.piranishvili1@gmail.com>
2024-02-06 04:04:15 -0800

    feat: add get method for region

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = new List<Region>
+            {
+                new Region
+                {
+                    Id= Guid.NewGuid(),
+                    Name="Auckland Region",
+                    Code="AKL",
+                    RegionImageUrl="image"
+                },
+                new Region
+                {
+                    Id= Guid.NewGuid(),
+                    Name="Wellington Region",
+                    Code="WLG",
+                    RegionImageUrl="image"
+                }
+            };
+            return Ok(regions);
+        }
+    }
+}
