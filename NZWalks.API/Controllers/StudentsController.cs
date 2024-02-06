873b122fe944bd44eadd1efddbc9d82810c50a5a
teonateona12 <teona.piranishvili1@gmail.com>
2024-02-06 03:14:19 -0800

    Add project files.

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "Teona", "Mariami", "Ana", "Mariami" };
+            return Ok(studentNames);
+        }
+    }
+}
