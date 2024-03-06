fbcc877f957d5d1546a88ca6c87518e5c70d6619
teonateona12 <teona.piranishvili1@gmail.com>
2024-03-06 12:22:41 +0400

    feat: seed roles in database

--- NZWalks.API/Data/NZWalksAuthDbContext.cs
+++ NZWalks.API/Data/NZWalksAuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace NZWalks.API.Data
@@ -8,6 +9,33 @@ namespace NZWalks.API.Data
         public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options): base(options) 
         { 
         }
-        
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "07bd1c7c-57e8-48c1-a52a-3ff845b5254c";
+            var writerRoleId = "ad9a340a-1787-4988-8476-4309f61f0a0c";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name="Reader",
+                    NormalizedName="Reader".ToUpper()
+
+                },
+                new IdentityRole
+                {
+                    Id=writerRoleId,
+                    ConcurrencyStamp=writerRoleId,
+                    Name="Writer",
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
