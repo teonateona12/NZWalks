c90e1080641829ef236c59726fac238b57be29fd
teonateona12 <teona.piranishvili1@gmail.com>
2024-03-24 13:39:00 +0400

    feat: add get regions method in MVC

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -14,7 +14,7 @@ namespace NZWalks.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
+//    [Authorize]
     public class RegionsController : ControllerBase
     {
         private readonly NZWalksDbContext dbContext;
---
+++ NZWalksUI/Controllers/RegionsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalksUI.Models.DTO;
+
+namespace NZWalksUI.Controllers
+{
+    public class RegionsController : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public RegionsController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<RegionsDto> response = new List<RegionsDto>();
+            var client = httpClientFactory.CreateClient();
+
+            var httpResponseMessage = await client.GetAsync("https://localhost:7160/api/regions");
+
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            response.AddRange(await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<RegionsDto>>());
+
+            return View(response);
+        }
+    }
+}
+ 
